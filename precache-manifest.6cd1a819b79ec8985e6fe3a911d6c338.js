37c9735121a540bd543bd326353dd944ecae9227
TobiasBregante <tobiasbegante2018@gmail.com>
2020-08-25 12:22:52 -0300

    first commit for maicki for your projects

---
+++ precache-manifest.6cd1a819b79ec8985e6fe3a911d6c338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aad3a64943222887b22e1739ab1b3c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f01807a2acba7cc510d1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01807a2acba7cc510d1",
+    "url": "/static/js/main.d718c870.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
